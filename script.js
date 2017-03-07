9d68ae09646eb0c3ac6f115f91e461370fc6690a
Steven Copeland <steven@thomasleecopeland.com>
2017-03-07 08:52:46 -0500

    deleting function
    deleting money left_to_earn to put in another file

--- script.js
+++ script.js
@@ -12,8 +12,3 @@ var ai1 = new player("AI1");
 var ai2 = new player("AI2");
 var ai3 = new player("AI3");
 var winner;
-
-var money_to_earn = function() {
-    var money_left_to_earn = 500000 - me.money
-    alert("$" + money_left_to_earn + " left to earn");
-}
