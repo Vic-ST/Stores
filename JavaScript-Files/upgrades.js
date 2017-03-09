a57d6e61a57b987ba61804388fa805488d6cd805
Steven Copeland <steven@thomasleecopeland.com>
2017-03-09 12:44:55 -0500

    adding some code
    adding some code and commenting it out.

--- JavaScript-Files/upgrades.js
+++ JavaScript-Files/upgrades.js
@@ -1,6 +1,22 @@
 // code examples for all upgrades
 
-var player = new Object()
+function player(name) {
+  	this.money = 15,
+  	this.name = name,
+	this.shares = 5,
+    this.worker = 0,'
+    this.employee = 0
+}
 
 var me = new player("Bob")
 var player2 = new player("Sam")
+
+/*
+Example:
+var buy = function(person) {
+    var buying = prompt("How many workers would you like to buy?");
+    var buying = function() {
+        person.money -= buying * 20
+    }
+}
+*/
