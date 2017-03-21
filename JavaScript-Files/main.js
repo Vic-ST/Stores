8cd3d4c94e664455b41290edeca59de330b59c66
Steven Copeland <steven@thomasleecopeland.com>
2017-03-21 16:46:48 -0400

    Deleting some lines of code

--- JavaScript-Files/main.js
+++ JavaScript-Files/main.js
@@ -3,7 +3,7 @@ function player(name) {
     this.properties = 1,
     this.workers = 0,
     this.name = name,
-    this.shares = 5;
+    this.shares = 5
 }
 
 var me = new player("You");
@@ -14,16 +14,3 @@ var ai2 = new player("AI2");
 var ai3 = new player("AI3");
 
 var winner;
-
-/*if(me.money >= 50) {
-    var buy_emp = prompt("Would you like to buy some employees? If so, please write yes in lowercase.");
-    if(buy_emp === "yes".toLowercase()) {
-        var buy_how_many = prompt("How many would you like to buy?");
-        employee += buy_how_many;
-        alert("You bought: " + buy_how_many);
-    }else{
-        alert("Okay then. No employees for you!")
-    }
-}
-
-var winner;*/
