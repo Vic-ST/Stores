1031ba9c053762ece3d7122bdb9720b9f1717ad1
Steven Copeland <steven@thomasleecopeland.com>
2017-03-22 07:51:22 -0400

    Adding a new person
    Adding a new person to main.

--- JavaScript-Files/main.js
+++ JavaScript-Files/main.js
@@ -9,8 +9,10 @@ function player(name) {
 var me = new player("You");
 var other = new player("Person 2");
 var person = new player("Person 3");
+var newPerson = new player("Person 4");
 var ai1 = new player("AI1");
 var ai2 = new player("AI2");
 var ai3 = new player("AI3");
+
 
 var winner;
