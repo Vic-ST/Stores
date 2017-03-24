ba21f8e2e5a4ea9f7b5363c294865405ed63d92c
Steven Copeland <steven@thomasleecopeland.com>
2017-03-23 21:18:03 -0400

    Changing code
    I had put the wrong code into the wrong branch so in this commit I am just transferring that code from this to that.

--- JavaScript-Files/jQuery.js
+++ JavaScript-Files/jQuery.js
@@ -1,44 +1,24 @@
 $(document).ready(function() {
     $(".rules").hide();
     $(".game").hide();
-    $(".2playerfunc").hide();
-    $(".3playerfunc").hide();
     $(".ruleButton").click(function() {
-        $(".homePage").fadeOut(1000);
+        $(".homePage").hide(1000);
         $(".rules").fadeIn(1000);
     });
     $(".back").click(function() {
-        $(".rules").fadeOut(1000);
-        $(".game").fadeOut(1000);
+        $(".rules").hide(1000);
+        $(".game").hide(1000);
         $(".homePage").fadeIn(1000);
     });
-    $(".back2").click(function() {
-        $(".rules").fadeOut(1000);
-        $(".2playerfunc").fadeOut(1000);
-        $(".game").fadeIn(1000);
-    });
-    $(".back3").click(function() {
-        $(".rules").fadeOut(1000);
-        $(".3playerfunc").fadeOut(1000);
-        $(".game").fadeIn(1000);
-    });
     $(".gameButton").click(function() {
-        $(".homePage").fadeOut(1000);
+        $(".homePage").hide(1000);
         $(".game").fadeIn(1000);
     });
     $(".hide").click(function() { 
         $(".hide, .back").fadeOut(1000);
     });
     $(".goTGame").click(function() {
-        $(".rules").fadeOut(1000);
+        $(".rules").hide(1000);
         $(".game").fadeIn(1000);
-    });
-    $(".2player").click(function() { 
-        $(".game").fadeOut(1000);
-        $(".2playerfunc").fadeIn(1000);
-    });
-    $(".3player").click(function() { 
-        $(".game").fadeOut(1000);
-        $(".3playerfunc").fadeIn(1000);
     });
 });
