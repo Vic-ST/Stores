9790f35f365d07af246c0c9fadb0633afebc9636
Steven Copeland <steven@thomasleecopeland.com>
2017-03-27 15:20:17 -0400

    Updating and renaming begin_round.js
    Updating begin_round.js by adding a new function and renaming it to. You may think: "Oh, no! Steven is commenting out all the code!". And as a matter of fact that is true! I am commiting this to a new branch and also will soon be adding new functions to make this work again.

--- JavaScript-Files/howManyPlayers.js
+++ JavaScript-Files/howManyPlayers.js
@@ -1,6 +1,19 @@
     var round = 0;
 
-    var beginRound = function() {
+    var howManyPlayers = function() {
+        howManyPlayersWanted = prompt("How many players would you like(you can only have 2-4 players)? Remeber that you have to write the digit name like this: 2 or like this: two");
+        if(howManyPlayersWanted === "2" || howManyPlayersWanted === "two") {
+            beginRoundWith2();
+        }else if(howManyPlayersWanted === "3" || howManyPlayersWanted === "three") {
+            beginRoundWith3();
+        }else if(howManyPlayersWanted === "4" || howManyPlayersWanted === "four") {
+            beginRoundWith4();
+        }else{
+            alert("Sorry! That number of player is not 2, 3, or 4. Remember you have to write the digit like: 2 or like: two.");
+            howManyPlayers();
+    };
+
+    /*var beginRound = function() {
         var rollDice = function() {
             return Math.floor(Math.random()*6);
         };
@@ -239,3 +252,4 @@
         };
         checkWinner();
     };
+*/
