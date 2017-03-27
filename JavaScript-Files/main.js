ab7896eb03db714c9f3a890629f26e669cf16067
Steven Copeland <steven@thomasleecopeland.com>
2017-03-27 15:04:27 -0400

    Adding a naming function
    Adding a naming function but commenting it out because I think I am doing it wrong.

--- JavaScript-Files/main.js
+++ JavaScript-Files/main.js
@@ -6,7 +6,13 @@ function player(name) {
     //this.shares = 5
 }
 
-var me = new player("You");
+/*
+var player_ask_name = function() {
+    player1 = prompt("Hello! What is your name? This name will be used for Player 1")
+}
+*/
+
+var me = new player("Person 1");
 var other = new player("Person 2");
 var person = new player("Person 3");
 var newPerson = new player("Person 4");
