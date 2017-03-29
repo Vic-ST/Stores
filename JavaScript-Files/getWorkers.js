64665701ad2bb6f50d408b13a0ca29d83b6602ab
Steven Copeland <steven@thomasleecopeland.com>
2017-03-29 19:08:45 -0400

    Editing the file
    Now I should be able to call the function from as different file and is should still work.

--- JavaScript-Files/getWorkers.js
+++ JavaScript-Files/getWorkers.js
@@ -1,20 +1,22 @@
-var youBuyWorkers = function() {
-            var ask = function() {
-                var max = Math.floor(me.money/20);
-                var askP = prompt("How many workers would you like to buy? You have " + me.money + " dollars. The most you can buy is " + max, max)*1;
-                if (me.money - (askP * 20) < 0) {
-                    confirm("Oops! Sorry, it looks like you don't have enough money.");
-                    ask();
-                }else if(isNaN(askP)) {
-                    alert("Sorry! That is not a number!");
-                    ask();
-                } else {
-                    var cost = askP*20;
-                    me.workers += askP;
-                    me.money -= cost;
-                    confirm("You bought " + me.workers + " workers");
-                }
-            };
-
+var buyWorkers = function(person1, person2, person3, person4) {
+	var ask = function(exPerson) {
+    	var max = Math.floor(exPerson.money/20);
+		var askP = prompt("How many workers would you like to buy? You have " + exPerson.money + " dollars. The most you can buy is " + max, max)*1;
+		if (exPerson.money - (askP * 20) < 0) {
+			confirm("Oops! Sorry, it looks like you don't have enough money.");
+			ask();
+		}else if(isNaN(askP)) {
+			alert("Sorry! That is not a number!");
             ask();
-        };
+		} else {
+			var cost = askP*20;
+			exPerson.workers += askP;
+			exPerson.money -= cost;
+			confirm("You bought " + exPerson.workers + " workers");
+  			}
+	};
+	ask(person1);
+	ask(person2);
+	ask(person3);
+	ask(person4);
+};
