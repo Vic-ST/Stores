06736821f84762a04e5397a7a97f3cf4f798b57f
Steven Copeland <steven@thomasleecopeland.com>
2017-03-29 14:56:45 -0400

    Changing the tab and adding parameters
    adding the parameter so that you can call the function on different players

--- JavaScript-Files/taxPayer.js
+++ JavaScript-Files/taxPayer.js
@@ -1,16 +1,16 @@
-var taxPayer = function() {
+var taxPayer = function(person1, person2, person3, person4, person5, person6, person7) {
 	round += 1;
 	var roundsLeftUntilTaxDay = 6 - round;
 	alert("The round is round " + round + " which means you have " + roundsLeftUntilTaxDay + " days until taxday.");
    	if(round === 6) {
        	alert("Tax day: Everybody loses 10% of their money.");
-       	newPerson.money *= 0.90;
-       	person.money *= 0.90;
-       	other.money *= 0.90;
-       	me.money *= 0.90;
-        ai1.money *= 0.90;
-        ai2.money *= 0.90;
-        ai3.money *= 0.90;
+       	person1.money *= 0.90;
+       	person2.money *= 0.90;
+       	person3.money *= 0.90;
+       	person4.money *= 0.90;
+        person5.money *= 0.90;
+        person6.money *= 0.90;
+        person7.money *= 0.90;
         round = 0;
     }
 };
