47ee7f4a9aa3b2c1e6e6d06b8c0e422ae6c21eee
Steven Copeland <steven@thomasleecopeland.com>
2017-03-29 15:01:01 -0400

    Deleting the taxPayer function
    Deleting the taxPayer function from here

--- JavaScript-Files/begin_round2.js
+++ JavaScript-Files/begin_round2.js
@@ -132,23 +132,7 @@ var beginRoundWith2 = function() {
         getMoneyM(ai2);
         getMoneyM(ai3);
         
-        var taxPayer = function() {
-            round += 1;
-            var roundsLeftUntilTaxDay = 6 - round;
-            alert("The round is round " + round + " which means you have " + roundsLeftUntilTaxDay + " days until taxday.");
-            if(round === 6) {
-                alert("Tax day: Everybody loses 10% of their money.");
-                //newPerson.money *= 0.90;
-                person.money *= 0.90;
-                other.money *= 0.90;
-                me.money *= 0.90;
-                //ai1.money *= 0.90;
-                ai2.money *= 0.90;
-                ai3.money *= 0.90;
-                round = 0;
-            }
-        };
-        taxPayer();
+        
         var checkWinner = function() {
             var list = [];
             var cMoney = function(pl) {
