9f4481d29554d7af2327e21366147c015553034d
Steven Copeland <steven@thomasleecopeland.com>
2017-03-31 14:31:22 -0400

    Deleting the called functions
    Deleting the function which has called statements.

--- JavaScript-Files/getMoney.js
+++ JavaScript-Files/getMoney.js
@@ -1,28 +1,21 @@
 var getMoneyW = function(p) {
-            pValue = p.workers * 5 + 5;
-            money = rollDice() * pValue;
-            p.money += money;
-        };
-        
-        var getMoneyM = function(pl) {
-            getMoneyW(pl);
-            getMoneyW(pl);
-            getMoneyW(pl);
-            getMoneyW(pl);
-            getMoneyW(pl);
-            //getMoneyW(pl);
-            //getMoneyW(pl);
-            if (pl === me) {
-                confirm(pl.name + " have " + pl.money + " dollars");
-            } else {
-                confirm(pl.name + " has " + pl.money + " dollars");
-            }
-            pl.workers = 0;
-        };
-        //getMoneyM(newPerson);
-        getMoneyM(other);
-        //getMoneyM(person);
-        getMoneyM(me);
-        getMoneyM(ai1);
-        getMoneyM(ai2);
-        getMoneyM(ai3);ge
+	pValue = p.workers * 5 + 5;
+	money = rollDice() * pValue;
+	p.money += money;
+};
+
+var getMoneyM = function(pl) {
+	getMoneyW(pl);
+	getMoneyW(pl);
+	getMoneyW(pl);
+	getMoneyW(pl);
+	getMoneyW(pl);
+	getMoneyW(pl);
+	getMoneyW(pl);
+	if (pl === me) {
+		confirm(pl.name + " have " + pl.money + " dollars");
+	} else {
+		confirm(pl.name + " has " + pl.money + " dollars");
+	}
+	pl.workers = 0;
+};
