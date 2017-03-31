232f4b3ccbfabca86948f0a2092386d5abb627c9
Steven Copeland <steven@thomasleecopeland.com>
2017-03-31 14:47:58 -0400

    Renaming the function
    Renaming the function because all the begin round functions had the exact same name!

--- JavaScript-Files/begin_round1.js
+++ JavaScript-Files/begin_round1.js
@@ -1,5 +1,5 @@
-    var beginRoundWith2 = function() {
-        var rollDice = function() {
-            return Math.floor(Math.random()*6);
-        };
+var beginRoundWith1 = function() {
+    var rollDice = function() {
+        return Math.floor(Math.random()*6);
     };
+};
