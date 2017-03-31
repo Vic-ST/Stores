3491ce97f7d3991b4fee18cbd006e161bdbd4480
Steven Copeland <steven@thomasleecopeland.com>
2017-03-31 19:21:32 -0400

    Adding the functions
    Calling all the functions for this file.

--- JavaScript-Files/begin_round1.js
+++ JavaScript-Files/begin_round1.js
@@ -1,2 +1,21 @@
-var beginRoundWith1 = function() {
+var beginRoundWith1 = function(checkWinner, getMoneyM, buyWorkers, buyWorkersComputer, taxPayer) {
+	checkWinner(me, ai1, ai2, ai3, "", "", "");
+	getMoneyM(me);
+	getMoneyM(ai1);
+	getMoneyM(ai2);
+	getMoneyM(ai3);
+	getMoneyM("");
+	getMoneyM("");
+	getMoneyM("");
+	buyWorkers(me);
+	buyWorkers("");
+	buyWorkers("");
+	buyWorkers("");
+	buyWorkersComputer(ai1);
+	buyWorkersComputer(ai2);
+	buyWorkersComputer(ai3);
+	taxPayer(me);
+	taxPayer(ai1);
+	taxPayer(ai2);
+	taxPayer(ai3);
 };
