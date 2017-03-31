43ad09ef02743c85aa23f25e852e5e33e4ce1018
Steven Copeland <steven@thomasleecopeland.com>
2017-03-31 14:40:50 -0400

    Editing the file
    Editing this file so it is ready for action.

--- JavaScript-Files/getWorkersComputer.js
+++ JavaScript-Files/getWorkersComputer.js
@@ -1,13 +1,9 @@
-        var buyWorkers = function(ai) {
-            if (Math.floor((ai.money - 5)/21) > 0) {
-                var toBuy = Math.floor((ai.money - 10)/20);
-                var cost = toBuy * 20;
-                ai.workers += toBuy;
-                ai.money -= cost;
-            }
-            confirm(ai.name + " bought " + ai.workers + " workers");
-        };
-        
-        buyWorkers(ai1);
-        buyWorkers(ai2);
-        buyWorkers(ai3);
+var buyWorkers = function(ai) {
+	if (Math.floor((ai.money - 5)/21) > 0) {
+		var toBuy = Math.floor((ai.money - 10)/20);
+		var cost = toBuy * 20;
+		ai.workers += toBuy;
+		ai.money -= cost;
+	}
+	confirm(ai.name + " bought " + ai.workers + " workers");
+};
