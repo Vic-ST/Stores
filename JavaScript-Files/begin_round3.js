3ab231d1c17382ccb9e6d2200d439b44add7b584
Steven Copeland <steven@thomasleecopeland.com>
2017-03-31 19:37:41 -0400

    Doing one of the last commits
    Finishing up all the commits.

--- JavaScript-Files/begin_round3.js
+++ JavaScript-Files/begin_round3.js
@@ -1,2 +1,18 @@
-var beginRoundWith3 = function() {
+var beginRoundWith3 = function(checkWinner, getMoneyM, buyWorkers, buyWorkersComputer, taxPayer) {
+	checkWinner(me, person, other, ai3, , "", "", "");
+	getMoneyM(me);
+	getMoneyM(person);
+	getMoneyM(other);
+	getMoneyM(ai3);
+	getMoneyM("");
+	getMoneyM("");
+	getMoneyM("");
+	buyWorkers(me);
+	buyWorkers(person);
+	buyWorkers(other);
+	buyWorkers("");
+	buyWorkersComputer("");
+	buyWorkersComputer("");
+	buyWorkersComputer(ai3);
+	taxPayer(me, person, other, ai3, "", "", "");
 };
