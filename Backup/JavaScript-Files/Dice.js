b2bd0196ae236be5abb16bcf585b01708bf56670
Steven Copeland <steven@thomasleecopeland.com>
2017-04-06 21:43:37 -0400

    Deleting backup folder.

--- Backup/JavaScript-Files/Dice.js
+++
@@ -1,3 +0,0 @@
-var rollDice = function() {
-	return Math.floor(Math.random()*6);
-};
--- Backup/JavaScript-Files/money_to_earn.js
+++
@@ -1,4 +0,0 @@
-var money_to_earn = function() {
-    var money_left_to_earn = 500000 - me.money;
-    alert("$" + money_left_to_earn + " left to earn");
-};
