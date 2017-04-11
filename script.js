178ad8c0523e5b73b623b92062d817228f3e50f4
Steven Copeland <steven@thomasleecopeland.com>
2017-04-11 13:54:01 -0400

    adding semi-colons
    Adding a lot of semi-colons into this page.

--- script.js
+++ script.js
@@ -82,17 +82,17 @@ var youBuyWorkers = function(pl) {
         pl.money -= ask*20;
         confirm(pl.name + " bought " + pl.workers + " workers");
     }
-}
+};
 var buyWorkers = function(ai) {
     max = Math.floor(ai.money/21);
     ai.workers += max;
     ai.money -= max*20;
     confirm(ai.name + " bought " + ai.workers + " workers");
-}
+};
 var getMoneyW = function(ply) {
     value = ply.workers * 5 + 5;
     ply.money += value * (rollDice()/2);
-}
+};
 var getMoneyM = function(pl) {
     getMoneyW(pl);
     getMoneyW(pl);
@@ -100,7 +100,7 @@ var getMoneyM = function(pl) {
     getMoneyW(pl);
     confirm(pl.name + " has " + pl.money);
     pl.workers = 0;
-}
+};
 var taxPayer = function() {
     round += 1;
     if (round === 6) {
@@ -116,7 +116,7 @@ var taxPayer = function() {
         var left = 6 - round;
         confirm("There are " + left + " days till taxday");
     }
-}
+};
 var cWinner = function(round) {
     for (var i = 0; i < playerList.length; i++) {
         if (playerList[i].money >= 500000) {
@@ -127,26 +127,26 @@ var cWinner = function(round) {
         winner1 = winnerList[0];
         round = function() {
             confirm(winner1.name + " won with " + winner1.money);
-        }
+        };
     } else if (winnerList.length === 2) {
         winner1 = winnerList[0];
         winner2 = winnerList[1];
         round = function() {
             confirm(winner1.name + " and " + winner2.name + " both won with over 500,000");
-        }
+        };
     } else if (winnerList.length === 3) {
         winner1 = winnerList[0];
         winner2 = winnerList[1];
         winner3 = winnerList[2];
         round = function() {
             confirm(winner1.name + ", " + winner2.name + ", and " + winner3.name + " all won with over 500,000");
-        }
+        };
     } else if (winnerList.length === 4) {
         round = function() {
             confirm("Everybody won with over 500,000");
-        }
+        };
     }
-}
+};
 //round functions
 
 var beginRound1 = function() {
@@ -163,7 +163,7 @@ var beginRound1 = function() {
     getMoneyM(ai3);
     taxPayer();
     cWinner(beginRound1);
-}
+};
 var beginRound2 = function() {
     if (player1.name === "#player1") {
         player1.name = prompt("What is the first player's name?");
@@ -179,7 +179,7 @@ var beginRound2 = function() {
     getMoneyM(ai3);
     taxPayer();
     cWinner(beginRound2);
-}
+};
 var beginRound3 = function() {
     if (player1.name === '#player1') {
         player1.name = prompt("What is the first player's name?");
@@ -196,7 +196,7 @@ var beginRound3 = function() {
     getMoneyM(ai1);
     taxpayer();
     cWinner(beginRound3);
-}
+};
 var beginRound4 = function() {
     if (player1.name === '#player1') {
         player1.name = prompt("What is the first player's name?");
@@ -214,4 +214,4 @@ var beginRound4 = function() {
     getMoneyM(player4);
     taxpayer();
     cWinner(beginRound4);
-}
+};
