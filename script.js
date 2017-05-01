10d10c528f628e74eaf8e167c4a7abdd92c9a4ef
Steven Copeland <steven@thomasleecopeland.com>
2017-05-01 11:40:21 -0400

    making string double quote
    Making the string with the hash tag double quote.

--- script.js
+++ script.js
@@ -223,7 +223,7 @@ var play = function() {
     document.getElementById("audio").innerHTML = "<audio controls autoplay repeat class='audio'><source src='Laser Groove.mp3' type='audio/mpeg'>Your browser does not support the audio element.</audio>";
     clicks = clicks + 1;
     if(clicks === 2) {
-        document.getElementById("audio").innerHTML = '#';
+        document.getElementById("audio").innerHTML = "#";
         clicks = 0;
     }
 };
